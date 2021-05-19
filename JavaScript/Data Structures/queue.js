5c4fb467eb21d8f65dffd8f2814fab6a634d3ca1
Jacob Varner <jacob.varner14@gmail.com>
2021-05-18 22:29:43 -0500

    Added JSDoc comments to both Queue and Stack data structures

--- JavaScript/Data Structures/queue.js
+++ JavaScript/Data Structures/queue.js
@@ -1,12 +1,27 @@
+/**
+ * A Queue data structure that creates a FIFO list of items.
+ *
+ * @param {Array} array the initial array or single item to intialize the queue with.
+ */
 function Queue(array = []) {
 	this.length = Array.isArray(array) ? array.length : 1;
 	this.array = Array.isArray(array) ? array : [array];
 
+	/**
+	 * Adds an item to the queue and increases the length by 1.
+	 *
+	 * @param {} value the value the add to the queue
+	 */
 	this.enqueue = (value) => {
 		this.array.push(value);
 		this.length++;
 	};
 
+	/**
+	 * Removes the first item added to the queue from the queue.
+	 *
+	 * @returns {}  the first value added to the queue
+	 */
 	this.dequeue = () => {
 		if (!this.length) return null;
 		let output = this.array.shift();
@@ -14,16 +29,31 @@ function Queue(array = []) {
 		return output;
 	};
 
+	/**
+	 * Returns the first value in the queue, but does not remove it from the queue.
+	 *
+	 * @returns {}  the first value in the queue
+	 */
 	this.first = () => {
 		if (!this.length) return null;
 		return this.array[0];
 	};
 
+	/**
+	 * Returns the last value in the queue, but does not remove it from the queue.
+	 *
+	 * @returns {}  the last item in the queue
+	 */
 	this.last = () => {
 		if (!this.length) return null;
 		return this.array[this.length - 1];
 	};
 
+	/**
+	 * Determines whether the queue only contains items of the same type.
+	 *
+	 * @returns {Boolean}   whether or not all items in the queue are of the same type
+	 */
 	this.isSameType = () => {
 		if (!this.length) return true;
 		let type = typeof this.array[0];
--- JavaScript/Data Structures/stack.js
+++ JavaScript/Data Structures/stack.js
@@ -1,18 +1,36 @@
+/**
+ * A Stack data structure that implements a LIFO list of items.
+ */
 function Stack() {
 	this.height = 0;
 	this.stack = [];
 
+	/**
+	 * Adds an item to the stack.
+	 *
+	 * @params {} value the value added to the top of the stack
+	 */
 	this.push = (val) => {
 		this.stack.push(val);
 		this.height++;
 	};
 
+	/**
+	 * Removes the top item from the stack.
+	 *
+	 * @returns {}  the item at the top of the stack.
+	 */
 	this.pop = () => {
 		if (this.height === 0) return null;
 		this.height--;
 		return this.stack.pop();
 	};
 
+	/**
+	 * Returns the top item from the stack but does not remove it.
+	 *
+	 * @returns {}  the item at the top of the stack.
+	 */
 	this.peek = () => {
 		if (this.height === 0) return null;
 		return this.stack[this.height - 1];
